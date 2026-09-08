2a36f2c94491ca0cbedd34764a83042fed29ec71
linhhv <linhhv@ftech.ai>
2026-09-08 18:34:08 +0700

    update fast api for house price prediction

--- House-Price_Prediction/app.py
+++ House-Price_Prediction/app.py
@@ -5,18 +5,49 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Dict
 
-
+import joblib
+# Load the trained model and scaler 
+model = joblib.load("artifacts/best_model.pkl")
+scaler = joblib.load("artifacts/scaler.pkl")
 app = FastAPI(title="California House Price Prediction API", version="1.0")
 
-# Load the trained model and scaler 
-with open("artifacts/best_model.pkl","rb") as f:
-    model = pickle.load(f)
-    
-with open("artifacts/scaler.pkl","rb") as f:
-    scaler = pickle.load(f)
     
 # =========================
 # Request schema
 # =========================
 
+class PredictionRequest(BaseModel):
+    data: Dict[str, float]
     
+# =========================
+# Home
+# =========================
+
+@app.get("/")
+
+def home():
+    return {"message": "Welcome to the California House Price Prediction API!"}
+
+
+# =========================
+# Prediction API
+# =========================
+
+@app.post("/predict_api")
+
+def predict_api(request: PredictionRequest):
+    data = request.data
+    print("Input:", data)
+    #Convert input --> numpy array
+    input_data = np.array(list(data.values())).reshape(1,-1) 
+    
+    #Scaling
+    
+    new_data = scaler.transform(input_data)
+    
+    #Prediction
+    output = model.predict(new_data)
+    prediction = float(output[0])
+    print("Prediction:", prediction)
+    
+    return {"prediction":prediction}
