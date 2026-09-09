a6a9a8c2938985f370c5dfcc96225207fc029b2e
linhhv <linhhv@ftech.ai>
2026-09-09 15:28:20 +0700

    complete california house price prediction

--- House-Price_Prediction/app.py
+++ House-Price_Prediction/app.py
@@ -4,13 +4,18 @@ import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Dict
-
+from fastapi.middleware.cors import CORSMiddleware
 import joblib
 # Load the trained model and scaler 
 model = joblib.load("artifacts/best_model.pkl")
 scaler = joblib.load("artifacts/scaler.pkl")
 app = FastAPI(title="California House Price Prediction API", version="1.0")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
     
 # =========================
 # Request schema
@@ -50,4 +55,26 @@ def predict_api(request: PredictionRequest):
     prediction = float(output[0])
     print("Prediction:", prediction)
     
-    return {"prediction":prediction}
+    return {
+        "prediction": prediction
+    }
+
+@app.post("/predict")
+def predict(request: PredictionRequest):
+    data = request.data
+    print("Input:", data)
+    #Convert input --> numpy array
+    input_data = np.array(list(data.values())).reshape(1,-1) 
+    
+    #Scaling
+    
+    new_data = scaler.transform(input_data)
+    
+    #Prediction
+    output = model.predict(new_data)
+    prediction = float(output[0])
+    print("Prediction:", prediction)
+    
+    return {
+        "prediction": prediction
+    }
