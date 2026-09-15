9098daec130a2f27a06b557c98a204c96dafec5a
LinhVuHoang <redtonlinh@gmail.com>
2026-09-15 23:49:13 +0700

    update: training pipeline

--- Diamond-Price-Prediction/src/DiamondPricePrediction/components/Data_ingestion.py
+++ Diamond-Price-Prediction/src/DiamondPricePrediction/components/Data_ingestion.py
@@ -20,7 +20,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Data ingestion started")
         try:
-            data = pd.read_csv("Diamond-Price-Prediction/Data/gemstone.csv")
+            data = pd.read_csv("Data/gemstone.csv")
             logging.info("Read the Data from the csv file")
             os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)), exist_ok=True)
             data.to_csv(self.ingestion_config.raw_data_path, index=False)
--- Diamond-Price-Prediction/src/DiamondPricePrediction/components/Model_trainer.py
+++ Diamond-Price-Prediction/src/DiamondPricePrediction/components/Model_trainer.py
@@ -18,7 +18,7 @@ class ModelTrainerConfig:
     
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer__config = ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
     
     def initate_model_training(self,train_array, test_array):
         
